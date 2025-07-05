2b1343d12a39ad491c627c5fc78d6deddda86eee
prg-maker <df3296709@gmail>
2025-07-05 20:45:33 -0300

    .

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 # ARQUIVO PRINCIPAL PARA RESOLUÇÃO DO TRABALHO (PROBLEMA 1):
+
+import math
+
+c = 0.041**(-1)
+
+class metodos:
+
+    def __init__(self):
+        y = 0 
+        self.eq =  c * (math.sqrt(1 + y**2) )
+    
+    def k1(x , y):
+        pass
+    def k2():
+        pass
+    def k3():
+        pass
+    def k4():
+        pass
+    
+    def runge_kutta(derivada , passo:float ,  x0 , y0):
+        
+        ...
+
+
+        # k1 = 
+        #k2   = 
+
+        #k3  = 
+ 
+        #k4 = 
