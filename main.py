f05b48d5026f57d209f135da73098d4049ed8cb9
prg-maker <df3296709@gmail>
2025-07-07 21:01:23 -0300

    refatorando o metodo runge-kutta

--- main.py
+++ main.py
@@ -9,45 +9,36 @@ class Metodos:
     
     def __init__(self):
         self.C = 0.041**(-1)
-        self.h = 0
-        self.k1 = 0
-        self.k2 = 0 
-        self.k3 = 0 
-        self.k4 = 0 
+        self.h = 0.01
         self.xy = []
+    def fun(self, x, y)->int:
+        result = (self.C * (math.sqrt(1 + y**2) ) )  * self.h
+        return result
 
-    def k1_calculate(self , x , y):
-        self.k1 = (self.C * (math.sqrt(1 + y**2) ) )  * self.h
-    def k2_calculate(self, x , y):
-        self.k2 =  (self.C * (math.sqrt(1 + (y+self.k1/2 )**2) ) )  * self.h
-    def k3_calculate(self, x , y):
-        self.k3 =  (self.C * (math.sqrt(1 + (y+self.k2/2 )**2) ) )  * self.h
-    def k4_calculate(self, x , y):
-        self.k4 =  (self.C * (math.sqrt(1 + (y+self.k3/2 )**2) ) )  * self.h
 
     def calculate_y(self , x , y )->int:
-        self.k1_calculate(x , y)
-        self.k2_calculate(x , y)
-        self.k3_calculate(x , y)
-        self.k4_calculate(x , y)
-
-        return y + ((self.h/ 6) *(self.k1 + 2*self.k2 + 2*self.k3 + self.k4))
+        k1 = self.fun(x , y)
+        k2 = self.fun(x+ self.h/2, y + k1/2)
+        k3 = self.fun(x + self.h/2 , y + k2/2)
+        k4 = self.fun(x + self.h/2 , y + k3/2)
+        y_ = y + ((self.h/ 6) *(k1 + 2*k2 + 2*k3 + k4))
+        return y_
     
     def runge_kutta(self,passo:float ,  x0 , y0 , x_final , parada):
-        self.h = passo
-        self.xy.append({"x0": x0 , "y0":y0})
+        
+        y_ = y0
+        
+        while True:
+            y_ = self.calculate_y(x0, y_)
 
-        yn = self.calculate_y(x=x0 , y=y0)
-        xn = 0
-        i = 0 
-        self.xy.append({f"x{i}": xn , f"y{i}": yn})
-        while xn + self.h/2 < x_final:
-            i+= 1
-            yn = self.calculate_y(x=xn, y=yn)
-            xn += self.h
-            self.xy.append({f"x{i}": xn , f"y{i}": yn})
+            x0 = x0+self.h
 
-        return { "xn":xn , "yn": yn}      
+            if abs(x_final - x0) <  parada:
+                break
+
+
+
+
     def plot_graph(self):
         x_vals = []
         y_vals = []
@@ -72,6 +63,6 @@ class Metodos:
 result =  Metodos()
 
 result.runge_kutta(passo=0.01 , x0=0 , y0=15 , x_final=20 , parada=  0.00001)
-result.plot_graph()
+#result.plot_graph()
 
 
